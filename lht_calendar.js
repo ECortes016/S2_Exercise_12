672dd61bd32490636fdce895af063666de2e8a15
Emmanuel Cortes <ecortes016@west-mec.org>
2019-02-25 17:26:23 -0800

    Created function
    Created two different functions for loop functions to show the days of the weeks

--- lht_calendar.js
+++ lht_calendar.js
@@ -40,6 +40,7 @@ document.getElementById('calendar').innerHTML = createCalendar(thisDay);
 function createCalendar(calDate) {
       var calendarHTML = "<table id='calendar_table'>";
       calendarHTML += calCaption(calDate);
+      calendarHTML += calWeekdayRow();
       calendarHTML += "</table>";
       return calendarHTML;
 }
@@ -57,4 +58,39 @@ function calCaption(calDate) {
 
       // Write the caption
       return "<caption>" + monthName[thisMonth] + " " + thisYear + "</caption>";
+}
+
+// Function to write a table row of weekday abbreviations
+
+function calWeekdayRow() {
+      // An array of weekday abbreviation
+      var dayName = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+      var rowHTML = "<tr>";
+
+      // Look through the day name array
+      for (var i = 0; i < dayName.length; i++) {
+            rowHTML += "<th class='calendar_weekdays'>" + dayName[i] + "</th>";
+      }
+
+      rowHTML += "</tr>";
+      return rowHTML;
+}
+
+// Function to calculate the number of days in the month
+
+function daysInMonth(calDate) {
+      // Array of days in each month
+      var dayCount = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+      // Going to extract the four digit year and month value
+      var thisYear = calDate.getFullYear();
+      var thisMonth = calDate.getMonth();
+
+      // Revise the days in February for leap year
+      if (thisYear % 4 === 0) {
+            dayCount[1] = 29
+      }
+
+      // Return the number of days for the current month
+      return dayCount[thisMonth];
 }
